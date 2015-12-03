c994787240d2a600e89c8b9b64a358e474a4aa31
Andrei Verdes <andrei.verdesh@gmail.com>
2015-12-03 02:56:13 +0200

    Improved bouncing and added listeners for swipe up and down

--- library/src/main/java/com/andreiverdes/HaiosView.java
+++ library/src/main/java/com/andreiverdes/HaiosView.java
@@ -1,56 +1,112 @@
 package com.andreiverdes;
 
+import android.animation.TimeInterpolator;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.support.v4.view.GestureDetectorCompat;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AnimationUtils;
 import android.widget.ScrollView;
 
-import timber.log.Timber;
+import java.lang.ref.WeakReference;
 
 /**
  * Created by andrei on 03/12/15.
  */
 public class HaiosView extends ScrollView {
-    private static final float MAX_Y_OVERSCROLL_DISTANCE = 200;
-    private int mMaxYOverscrollDistance ;
 
-    public HaiosView(Context context) {
+    private static final int WIDTH_DEVIDER_OVERSCROLL_DISTANCE = 2;
+    private static final float BOUNCE_ANIM_TIME = 300f;
+
+    private GestureDetectorCompat mGestureDetector;
+    private GesturesDetectorListener mGesturesDetectorListener;
+    private TimeInterpolator mInterpolator;
+
+    private boolean mTopHit;
+    private boolean mBottomHit;
+    private int mMaxOverScrollDistance;
+    private long mStartTime;
+
+    public HaiosView(final Context context) {
         super(context);
-        this.initBounceListView(context);
+        init();
     }
 
-    public HaiosView(Context context, AttributeSet attrs) {
+    public HaiosView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
-        this.initBounceListView(context);
+        init();
     }
 
-    public HaiosView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public HaiosView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        this.initBounceListView(context);
+        init();
     }
 
-    private void initBounceListView(Context pContext)
-    {
-        setOverScrollMode(OVER_SCROLL_ALWAYS);
-        //get the density of the screen and do some maths with it on the max overscroll distance
-        //variable so that you get similar behaviors no matter what the screen size
-
-        final DisplayMetrics metrics = pContext.getResources().getDisplayMetrics();
-        final float density = metrics.density;
-
-        mMaxYOverscrollDistance = (int) (density * MAX_Y_OVERSCROLL_DISTANCE);
+    private void init() {
+        this.mTopHit = false;
+        this.mBottomHit = false;
+        this.mGesturesDetectorListener = new GesturesDetectorListener(this);
+        this.mGestureDetector = new GestureDetectorCompat(getContext(), mGesturesDetectorListener);
+        this.setOverScrollMode(OVER_SCROLL_ALWAYS);
+        final int heightPixels = getContext().getResources().getDisplayMetrics().heightPixels;
+        mMaxOverScrollDistance = heightPixels / WIDTH_DEVIDER_OVERSCROLL_DISTANCE;
+        mInterpolator = new AccelerateDecelerateInterpolator();
     }
 
-    @Override protected boolean overScrollBy(int deltaX, int deltaY, int scrollX, int scrollY, int scrollRangeX, int scrollRangeY, int maxOverScrollX, int maxOverScrollY, boolean isTouchEvent) {
-        boolean returned = super.overScrollBy(deltaX, deltaY, scrollX, scrollY, scrollRangeX, scrollRangeY, maxOverScrollX, mMaxYOverscrollDistance, isTouchEvent);
-        Timber.i(""+this.canScrollVertically(-1)+" "+canScrollVertically(1));
-        return returned;
+    public void setSwipeListener(ISwipeListener pSwipeListener) {
+        this.mGesturesDetectorListener.setSwipeListener(pSwipeListener);
+    }
+
+    @Override protected void onScrollChanged(int scollX, int scrollY, int oldScrollX, int oldScrollY) {
+        super.onScrollChanged(scollX, scrollY, oldScrollX, oldScrollY);
+        View childView = this.getChildAt(0);
+        if(childView == null){
+            return;
+        }
+        if(getScrollY() <= 0){
+            this.mTopHit = true;
+            this.mBottomHit = false;
+        } else if(getScrollY() >= (childView.getHeight()-this.getHeight())){
+            this.mTopHit = false;
+            this.mBottomHit = true;
+        } else {
+            this.mTopHit = false;
+            this.mBottomHit = false;
+        }
+    }
+
+    @Override public boolean dispatchTouchEvent(MotionEvent ev) {
+        this.mGestureDetector.onTouchEvent(ev);
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override public boolean canScrollVertically(int direction) {
+        return true;
+    }
+
+    @Override protected boolean overScrollBy(int deltaX, int deltaY, int scrollX, int scrollY,
+                                             int scrollRangeX, int scrollRangeY, int maxOverScrollX,
+                                             int maxOverScrollY, boolean isTouchEvent) {
+        int overScrollDistance = mMaxOverScrollDistance;
+        if (isTouchEvent) {
+            mStartTime = AnimationUtils.currentAnimationTimeMillis();
+        } else {
+            final long elapsedTime = AnimationUtils.currentAnimationTimeMillis() - mStartTime;
+            float interpolation = mInterpolator.getInterpolation((float) elapsedTime / BOUNCE_ANIM_TIME);
+            interpolation = interpolation > 1 ? 1 : interpolation;
+            overScrollDistance -= overScrollDistance * interpolation;
+            overScrollDistance = overScrollDistance < 0 ? 0 : overScrollDistance;
+        }
+
+        return super.overScrollBy(deltaX, deltaY, scrollX, scrollY, scrollRangeX, scrollRangeY, maxOverScrollX, overScrollDistance, isTouchEvent);
     }
 
     @TargetApi(21)
-    @Override
-    public boolean dispatchNestedFling(float velocityX, float velocityY, boolean consumed) {
+    @Override public boolean dispatchNestedFling(float velocityX, float velocityY, boolean consumed) {
         // Not consumed means it wasn't handled because ScrollView
         // doesn't take over scrolling bounds into scroll range,
         // so we fling it ourselves to get it bounce back
@@ -61,5 +117,90 @@ public class HaiosView extends ScrollView {
         } else {
             return super.dispatchNestedFling(velocityX, velocityY, consumed);
         }
+    }
+
+    @Override protected void onDetachedFromWindow() {
+        this.mGestureDetector = null;
+        this.mGesturesDetectorListener = null;
+        super.onDetachedFromWindow();
+    }
+
+    public boolean isTopHit() {
+        return mTopHit;
+    }
+
+    public boolean isBottomHit() {
+        return mBottomHit;
+    }
+
+    public interface ISwipeListener{
+        void onSwipeUp(HaiosView pHaiosView);
+        void onSwipeDown(HaiosView pHaiosView);
+    }
+
+    public static class GesturesDetectorListener extends GestureDetector.SimpleOnGestureListener{
+
+        private static final int SWIPE_THRESHOLD = 150;
+        public static final int SWIPE_VELOCITY_THRESHOLD = 150;
+
+        private WeakReference<HaiosView> mHaiosViewWeakReference;
+        private WeakReference<ISwipeListener> mSwipeListenerWeakReference;
+
+        public GesturesDetectorListener(HaiosView pHaiosView){
+            this.mHaiosViewWeakReference = new WeakReference<>(pHaiosView);
+        }
+
+        @Override public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            this.onSwipeRight();
+                        } else {
+                            this.onSwipeLeft();
+                        }
+                    }
+                    result = true;
+                } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        this.onSwipeDown();
+                    } else {
+                        this.onSwipeUp();
+                    }
+                }
+                result = true;
+
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+
+        public void setSwipeListener(ISwipeListener pSwipeListener){
+            this.mSwipeListenerWeakReference = new WeakReference<>(pSwipeListener);
+        }
+
+        private void onSwipeUp() {
+            if(mHaiosViewWeakReference.get() != null && mHaiosViewWeakReference.get().isBottomHit()){
+                if(mSwipeListenerWeakReference != null && mSwipeListenerWeakReference.get() != null){
+                    this.mSwipeListenerWeakReference.get().onSwipeUp(mHaiosViewWeakReference.get());
+                }
+            }
+        }
+
+        private void onSwipeDown() {
+            if(mHaiosViewWeakReference.get() != null && mHaiosViewWeakReference.get().isTopHit()){
+                if(mSwipeListenerWeakReference != null && mSwipeListenerWeakReference.get() != null){
+                    mSwipeListenerWeakReference.get().onSwipeDown(mHaiosViewWeakReference.get());
+                }
+            }
+        }
+
+        private void onSwipeLeft() {}
+
+        private void onSwipeRight() {}
     }
 }
